89ab3c621fee8f98ec28dbefc9c9c780cb358e7e
Victor Lin <vlin@ufl.edu>
2016-10-31 17:27:40 -0400

    Fix data types, move constraints
    constraints caused problems with cx_Oracle’s cursor.execute() using
    parameters, so we’ll add them separately after tables are populated.

--- LAPDStopData/schema.sql
+++ LAPDStopData/schema.sql
@@ -1,27 +1,21 @@
 CREATE TABLE Officer (
     officer_id  NUMBER(5) NOT NULL,
     div_id      VARCHAR(2),
-    div_name    VARCHAR2(50),
-    PRIMARY KEY (officer_id)
+    div_name    VARCHAR2(50)
 )
 
 CREATE TABLE PoliceStop (
     stop_id            NUMBER(10) NOT NULL,
-    stop_date          DATE,
-    stop_type          VARCHAR2(50),
-    post_stop_activity VARCHAR2(5),
+    stop_date          TIMESTAMP(6),
+    stop_type          VARCHAR2(3),
+    post_stop_activity VARCHAR2(1),
     officer1_id        NUMBER(10),
-    officer2_id        NUMBER(10),
-    PRIMARY KEY (stop_id),
-    FOREIGN KEY (officer1_id) REFERENCES Officer(officer_id),
-    FOREIGN KEY (officer2_id) REFERENCES Officer(officer_id)
+    officer2_id        NUMBER(10)
 )
 
 CREATE TABLE Offender (
     offender_id NUMBER(10) NOT NULL,
     gender      VARCHAR2(1),
-    ethnicity   VARCHAR2(10),
-    stop_id     NUMBER(10),
-    PRIMARY KEY (offender_id),
-    FOREIGN KEY (stop_id) REFERENCES PoliceStop(stop_id)
+    ethnicity   VARCHAR2(20),
+    stop_id     NUMBER(10)
 )

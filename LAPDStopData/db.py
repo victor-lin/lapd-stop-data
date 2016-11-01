e259d68297a3deeea7c441af8fcb9feb97eba471
Victor Lin <vlin@ufl.edu>
2016-10-31 20:15:37 -0400

    Complete DB initialization
    Using cx_Oracle’s cursor.executemany() is much faster, also strips the
    need to convert values to Oracle-friendly strings.

--- LAPDStopData/db.py
+++ LAPDStopData/db.py
@@ -1,6 +1,5 @@
 from pandas import read_csv, to_datetime
-from pandas.tslib import Timestamp
-from numpy import int64, float64, isnan
+from numpy import float64, isnan
 
 from collections import OrderedDict
 from cx_Oracle import connect, makedsn, DatabaseError
@@ -26,15 +25,15 @@ def connect_db():
 def create_schema():
     """Create database schema"""
     with connect_db() as con:
-        with closing(con.cursor()) as cur:
-            with app.open_resource('schema.sql') as f:
-                log.info('Creating database schema')
-                schema_string = "CREATE SCHEMA AUTHORIZATION {} {}"
-                try:
-                    cur.execute(schema_string.format(app.config['UNAME'],
-                                                     f.read()))
-                except DatabaseError as db_error:
-                    log.error(db_error)
+        cur = con.cursor()
+        with app.open_resource('schema.sql') as f:
+            log.info('Creating database schema')
+            schema_string = "CREATE SCHEMA AUTHORIZATION {} {}"
+            try:
+                cur.execute(schema_string.format(app.config['UNAME'],
+                                                 f.read()))
+            except DatabaseError as db_error:
+                log.error(db_error)
         con.commit()
 
 
@@ -42,25 +41,30 @@ def populate(table):
     """Insert rows for specified table"""
     log.info('Inserting rows into table %s', table)
     with connect_db() as con:
-        with closing(con.cursor()) as cur:
-            log.info('Parsing data file...')
-            df_raw = read_csv(app.config['DATA'])
-            df_raw['STOP_DATE'] = to_datetime(df_raw.STOP_DT + df_raw.STOP_TM,
-                                              format='%m/%d/%Y%H:%M')
-            if table == 'Officer':
-                statements = get_insert_statements_officer(df_raw)
-            elif table == 'PoliceStop':
-                statements = get_insert_statements_stop(df_raw)
-            elif table == 'Offender':
-                statements = get_insert_statements_offender(df_raw)
-            else:
-                raise Exception('{} not a valid table name'.format(table))
-            log.info('Generating statements...')
-            statements = list(statements)
-            for i, statement in enumerate(statements):
-                log.debug('Executing (%i/%i): "%s"',
-                          i, len(statements), statement)
-                cur.execute(statement)
+        cur = con.cursor()
+        log.info('Parsing data file...')
+        df_raw = read_csv(app.config['DATA'])
+        df_raw['STOP_DATE'] = to_datetime(df_raw.STOP_DT + df_raw.STOP_TM,
+                                          format='%m/%d/%Y%H:%M')
+        log.info('Getting insert parameters...')
+        if table == 'Officer':
+            num_cols = 3
+            params = get_insert_params_officer(df_raw)
+        elif table == 'PoliceStop':
+            num_cols = 6
+            params = get_insert_params_stop(df_raw)
+        elif table == 'Offender':
+            num_cols = 4
+            params = get_insert_params_offender(df_raw)
+        else:
+            raise Exception('{} not a valid table name'.format(table))
+        log.info('Preparing statements...')
+        cur.prepare(INSERT_STR.format(table,
+                                      ','.join([':' + str(x + 1)
+                                                for x in range(num_cols)])))
+        log.info('Executing statements...')
+        cur.executemany(None, list(params))
+        log.info('Committing DB')
         con.commit()
 
 
@@ -81,21 +85,16 @@ def add_constraints():
 
 
 def convert_val(val):
-    """Return Oracle-friendly string based on data type"""
-    if type(val) is Timestamp:
-        date = val.strftime('%m/%d/%Y %I:%M')
-        return "TO_DATE('{}', 'mm/dd/yyyy hh:mi')".format(date)
+    """Return value based on data type"""
     if type(val) in {float64, float}:
         if isnan(val):
-            return 'NULL'
-        return str(int(val))
-    if type(val) is int64:
-        return str(val)
-    return "'{}'".format(val)
+            return None
+        return int(val)
+    return val
 
 
-def get_insert_statements_officer(df_raw):
-    """Return insert statements for Officer table"""
+def get_insert_params_officer(df_raw):
+    """Return ordered iterable of values for each row in Officer"""
     officer_cols = OrderedDict([('OFFICER_ID', ('OFCR1_SERL_NBR',
                                                 'OFCR2_SERL_NBR')),
                                 ('DIV_ID', ('OFCR1_DIV_NBR',
@@ -109,28 +108,33 @@ def get_insert_statements_officer(df_raw):
     df_all = df_officer1.append(df_officer2)
     df_all = df_all.groupby('OFFICER_ID').first().reset_index()[officer_cols.keys()]
     for i, row in df_all.iterrows():
-        row_vals = [convert_val(val) for val in row]
-        yield INSERT_STR.format('Officer', ','.join(row_vals))
+        yield [convert_val(val) for val in row]
 
 
-def get_insert_statements_stop(df_raw):
-    """Return insert statements for PoliceStop table"""
-    stop_dict = OrderedDict([('STOP_ID', 'FORM_REF_NBR'),
+def get_insert_params_stop(df_raw):
+    """Return ordered iterable of values for each row in PoliceStop"""
+    stop_cols = OrderedDict([('STOP_ID', 'FORM_REF_NBR'),
                              ('STOP_DATE', 'STOP_DATE'),
                              ('STOP_TYPE', 'STOP_TYPE'),
                              ('POST_STOP_ACTIVITY', 'POST_STOP_ACTV_IND'),
                              ('OFFICER1_ID', 'OFCR1_SERL_NBR'),
                              ('OFFICER2_ID', 'OFCR2_SERL_NBR')])
-    df_stop = df_raw.groupby('FORM_REF_NBR').first().reset_index()[stop_dict.values()]
-    df_stop.columns = stop_dict.keys()
+    df_stop = df_raw.groupby('FORM_REF_NBR').first().reset_index()[stop_cols.values()]
+    df_stop.columns = stop_cols.keys()
     for i, row in df_stop.iterrows():
-        row_vals = [convert_val(val) for val in row]
-        yield INSERT_STR.format('PoliceStop', ','.join(row_vals))
+        yield [convert_val(val) for val in row]
 
 
-def get_insert_statements_offender(df_raw):
-    """Return insert statements for PoliceStop table"""
-    pass
+def get_insert_params_offender(df_raw):
+    """Return ordered iterable of values for each row in Offender"""
+    offender_cols = OrderedDict([('OFFENDER_ID', 'STOP_NBR'),
+                                 ('GENDER', 'PERSN_GENDER_CD'),
+                                 ('ETHNICITY', 'DESCENT_DESC'),
+                                 ('STOP_ID', 'FORM_REF_NBR')])
+    df_offender = df_raw[offender_cols.values()]
+    df_offender.columns = offender_cols.keys()
+    for i, row in df_offender.iterrows():
+        yield [convert_val(val) for val in row]
 
 
 def drop_tables():

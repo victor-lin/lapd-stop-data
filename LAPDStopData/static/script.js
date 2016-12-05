db4a4c272a0d632935f9c86cd5a5599711fa7d1b
Victor Lin <vlin@ufl.edu>
2016-12-05 15:28:37 -0500

    Generate n random colors

--- LAPDStopData/static/script.js
+++ LAPDStopData/static/script.js
@@ -2,7 +2,6 @@
 function sendRequest(ext) {
     $.post(ext, function(data) {
         console.log("Sending POST to " + ext);
-        return data;
     });
 }
 
@@ -30,3 +29,90 @@ $(document).on('click', '#insertOffenders:enabled', function(){
 $(document).on('click', '#dropTables:enabled', function(){
     sendRequest('/_drop_tables/');
 });
+
+// return string in RGBa notation
+function hsvToRgb(h, s, v) {
+  var r, g, b;
+  var i;
+  var f, p, q, t;
+
+  // Make sure our arguments stay in-range
+  h = Math.max(0, Math.min(360, h));
+  s = Math.max(0, Math.min(100, s));
+  v = Math.max(0, Math.min(100, v));
+
+  // We accept saturation and value arguments from 0 to 100 because that's
+  // how Photoshop represents those values. Internally, however, the
+  // saturation and value are calculated from a range of 0 to 1. We make
+  // That conversion here.
+  s /= 100;
+  v /= 100;
+
+  if (s == 0) {
+    // Achromatic (grey)
+    r = g = b = v;
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
+  h /= 60; // sector 0 to 5
+  i = Math.floor(h);
+  f = h - i; // factorial part of h
+  p = v * (1 - s);
+  q = v * (1 - s * f);
+  t = v * (1 - s * (1 - f));
+
+  switch (i) {
+    case 0:
+      r = v;
+      g = t;
+      b = p;
+      break;
+
+    case 1:
+      r = q;
+      g = v;
+      b = p;
+      break;
+
+    case 2:
+      r = p;
+      g = v;
+      b = t;
+      break;
+
+    case 3:
+      r = p;
+      g = q;
+      b = v;
+      break;
+
+    case 4:
+      r = t;
+      g = p;
+      b = v;
+      break;
+
+    default: // case 5:
+      r = v;
+      g = p;
+      b = q;
+  }
+
+  rgb_str = "rgba(";
+  rgb_str += Math.round(r * 255) + ",";
+  rgb_str += Math.round(g * 255) + ",";
+  rgb_str += Math.round(b * 255) + ",";
+  rgb_str += "1)"
+  return rgb_str;
+};
+
+// generate n contrasting colors
+// return: list color strings in RGBa notation
+function generateColors(num_colors) {
+    var interval = 360 / (num_colors - 1);
+    var colors = [];
+    for (var i = 0; i < num_colors; i++) {
+        colors.push(hsvToRgb(interval * i, 80, 90));
+    }
+    return colors;
+}
